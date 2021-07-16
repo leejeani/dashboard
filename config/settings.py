5d0cdf3d8e02eb7df451fd3a6d391efdd16d3099
SanghunOh <otter.oh@gmail.com>
2021-07-16 12:53:11 +0900

    20210715-1 JMLEE .....

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6)wk)-4214o(tx#&%wfjfudv@fo$me3qcwl#1lvqu8ps+pp+_k'
+SECRET_KEY = 'Z0b!J9(cij=`LC1]-F[K<{[f?o?sEhm)Tp19F2./I`dGCjibt^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
